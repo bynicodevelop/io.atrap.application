ebfa72da5cc2359e64f674f15c47c260cacec238
nico-develop <contact@nico-develop.fr>
2022-01-17 14:38:23 +0100

    Initial commit

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="absolute bottom-0 left-0 w-full text-center py-3 text-xs italic">
+      Copyright © {new Date().getFullYear()} @Trap.io
+    </footer>
+  );
+}
